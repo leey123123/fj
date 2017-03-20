43f73c25939f55479982e374df19ff3e43ff48b4
liyao <394967420@qq.com>
2017-03-20 21:18:59 +0800

    提交更改

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,7 +1,26 @@
+import './mock/mockdata'
+import Vue from 'vue'
+import router from './router'
+import VueReource from 'vue-resource'
+import App from './app'
+import './css/comm.css'
+Vue.use(VueReource);
+Vue.config.productionTip = false;
+
+Vue.http.interceptors.push((request, next) => {
+  request.method = 'POST';
+  request.emulateJSON=true;
+　next((response) => {
+    　　response.body = '...';
+　　　　return response;
+
+  });
+});
+
 var env = process.env.NODE_ENV
 var baseConf = {};
 if(env === 'production'){
-  baseConf.url = "123";
+  baseConf.url = "";
 }else{
   baseConf.url = "";
 }
@@ -11,14 +30,8 @@ baseConf.option = {
                     data:{},
                     emulateJSON: true
                     };
-import './mock/mockdata'
-import Vue from 'vue'
-import router from './router'
-import VueReource from 'vue-resource'
-import App from './app'
-import './css/comm.css'
-Vue.use(VueReource);
-Vue.config.productionTip = false;
+
+
 window.baseConf = baseConf;
 
 /* eslint-disable no-new */
