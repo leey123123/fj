c4e2d1adc5a2ced2b516f305a0a2dd25c1e18919
liyao498 <liyao498@pingan.com.cn>
2017-03-20 15:02:24 +0800

    提交demo

---
+++ demo/aha/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <router-view></router-view>
+</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		
+
+	}
+
+</script>
---
+++ demo/aha/components/part/banner.vue
@@ -0,0 +1,12 @@
+<template>
+	<section class="y-epl-banner">
+			<router-link to="/introduction"><img src="../../assets/images/banner2.jpg" alt=""/></router-link>
+		</section>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name:'banner'
+	}
+
+</script>
