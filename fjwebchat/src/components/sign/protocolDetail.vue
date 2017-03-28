929c4ed352876ea0f6468297d9ee9d7a74ffb8b2
liyao498 <liyao498@pingan.com.cn>
2017-03-28 15:00:15 +0800

    提交文件夹

--- fjwebchat/src/components/sign/protocolDetail.vue
+++ fjwebchat/src/components/sign/protocolDetail.vue
@@ -57,20 +57,25 @@
         <div class="bind-wx-button">
             <a href="javascript:;" class="weui-btn weui-btn_default confirm-button" @click="showSign()">签署协议</a>
         </div>
-    </div>  
-     <div id="signature-pad" class="m-signature-pad">
-        <div class="m-signature-pad--body">
-          <canvas id="signcan"></canvas>
-        </div>
-        <div class="m-signature-pad--footer">
-          <div class="left" @click="clear()">
-            <a href="javascript:void(0);">清除</a>
+    </div> 
+    <div class="zzclass" @click="showSign()" v-show="popupVisible"></div> 
+    <transition name="popup-slide-bottom"
+    v-on:after-enter="afterEnter"
+    v-on:leave="leave">
+         <div id="signature-pad" class="m-signature-pad" v-show="popupVisible">
+            <div class="m-signature-pad--body">
+              <canvas id="signcan"></canvas>
+            </div>
+            <div class="m-signature-pad--footer">
+              <div class="left" @click="clear()">
+                <a href="javascript:void(0);">清除</a>
+              </div>
+              <div class="right" @click="signBase64()">
+                <a href="javascript:void(0);">签署</a>
+              </div>
+            </div>
           </div>
-          <div class="right" @click="signBase64()">
-            <a href="javascript:void(0);">签署</a>
-          </div>
-        </div>
-      </div>
+      </transition>
 </div>
 </template>
 <script>
@@ -79,28 +84,17 @@ export default{
     data:function(){
         return {
             popupVisible:false,
-            signaturePad:""
+            signaturePad:"",
+            tran:""
         }
     },
     methods:{
-        sign:function(){
-            var canvas = document.getElementById('signcan');
-            this.resizeCanvas(canvas);
-            this.signaturePad = new sign(canvas,{
-                minWidth:2,
-                maxWidth:5
-            });
-        },
-        showSign:function(){
-            if(this.signaturePad===""||this.signaturePad===undefined){
-
-                setTimeout(this.sign,1000);
-                
-            }
+         showSign:function(){
+            this.popupVisible =!this.popupVisible
         },
         resizeCanvas:function(canvas) {
 
-            var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+           var ratio =  Math.max(window.devicePixelRatio || 1, 1);
             canvas.width = canvas.offsetWidth * ratio;
             canvas.height = canvas.offsetHeight * ratio;
             canvas.getContext("2d").scale(ratio, ratio);
@@ -114,18 +108,43 @@ export default{
             } else {
                 console.log(this.signaturePad.toDataURL());
             }
-        }
+        },
+        afterEnter:function(){
+            if(this.signaturePad===""||this.signaturePad===undefined){
+                var canvas = document.getElementById('signcan');
+                this.resizeCanvas(canvas);
+                this.signaturePad = new sign(canvas,{
+                    minWidth:2,
+                    maxWidth:5
+                });
+                
+            }
+            
+        },
+        leave:function(){
+            this.clear();
+        },
+
+
     },
     created:function(){
-        this.showSign();
+        //this.showSign();
     }
 }
 
 </script>
 
 <style>
-    
-.m-signature-pad {
+.zzclass{
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    z-index: 5;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(27, 22, 22, 0.16);
+}
+.popup-slide-bottom {
   position: fixed;
   width: 100%;
   height: 40%;
@@ -134,6 +153,54 @@ export default{
   background-color: #fff;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.08) inset;
   border-radius: 4px;
+   -webkit-transform: translate3d(0, 0, 0);
+        transform: translate3d(0, 0, 0); 
+}
+
+
+.popup-slide-bottom-enter-active {
+  animation-name: fold-in;
+  animation-duration: .4s;
+}
+.popup-slide-bottom-leave-active {
+  animation-name: fold-out;
+  animation-duration: .4s;
+}
+@keyframes fold-in {
+  0% {
+    transform: translate3d(0, 100%, 0);
+    -webkit-transform: translate3d(0, 100%, 0);
+  }
+  100% {
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0);
+  }
+}
+@keyframes fold-out {
+  0% {
+    transform: translate3d(0, 0, 0);
+    -webkit-transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, 100%, 0);
+    -webkit-transform: translate3d(0, 100%, 0);
+  }
+}
+
+    
+.m-signature-pad {
+  position: fixed;
+  z-index: 10;
+  width: 100%;
+  height: 40%;
+  bottom: 0;
+  border: 1px solid #e8e8e8;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.08) inset;
+  border-radius: 4px;
+  -webkit-transform: translate3d(0, 0, 0);
+          transform: translate3d(0, 0, 0);
+
 }
 
 .m-signature-pad:before, .m-signature-pad:after {
@@ -169,7 +236,7 @@ export default{
   right: 20px;
   top: .1rem;
   bottom: 1.2rem;
-  border: 1px solid #f4f4f4;
+  border: solid 1px rgba(111, 90, 90, 0.51);
 }
 
 .m-signature-pad--body
