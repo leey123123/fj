61103ba21a1af3cc54907d37828fad37d9519481
liyao498 <liyao498@pingan.com.cn>
2017-05-16 15:30:44 +0800

    提交代码

--- qdhk/build/webpack.config.js
+++ qdhk/build/webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
     // 输出配置
     output: {
         // 输出路径是 myProject/output/static
-        path: path.resolve(__dirname, '../output/static'),
-        publicPath: 'static/',
+        path: path.resolve(__dirname, '../output'),
+        publicPath: '',
         filename: '[name].[hash].js'
         /*,
         chunkFilename: '[id].[chunkhash].js'*/
@@ -92,7 +92,7 @@ module.exports = {
         }),
 
         new HtmlWebpackPlugin({
-            filename: '../index.html',
+            filename: 'index.html',
             template: path.resolve(__dirname, '../src/index.html'),
             inject: true
         }),
