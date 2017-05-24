169d32af9e6196822805ec926ff03b752cfed9a1
liyao498 <liyao498@pingan.com.cn>
2017-05-24 16:58:02 +0800

    提交文件

--- qdhk/src/components/add.vue
+++ qdhk/src/components/add.vue
@@ -156,6 +156,7 @@ export default{
               code:''
             },
             applyMes:{
+              applyid:'',
               employtype:'',
               employtypecode:'',
               customername:'',
